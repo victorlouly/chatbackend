a58f884864c4bc7ecf458d3f2f2efc198c37c2ac
victorlouly <91222566+victorlouly@users.noreply.github.com>
2023-05-08 00:28:03 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chatfrontend-eta.vercel.app/",
+        origin: "http://localhost:3000",
         methods: ['GET', 'POST'],
     }
 });
